afaa0cab48138849968368f6bcd65f17e8c5a78f
bh487486-pixel <bh487486@gmail.com>
2026-05-14 15:26:16 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -169,3 +169,19 @@ async def main():
             await asyncio.sleep(60)
 
 asyncio.run(main())
+import asyncio
+import os
+import requests
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+async def prueba():
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    data = {
+        "chat_id": CHAT_ID,
+        "text": "Bot activo 🚀"
+    }
+    requests.post(url, data=data)
+
+asyncio.run(prueba())
